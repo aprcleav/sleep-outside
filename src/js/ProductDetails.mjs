12170f938d610dd22283cead8bd4d2edf6f697ca
reidsana <oksanareid48@gmail.com>
2026-07-14 20:14:55 -0500

    Add discount to product detail pages

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,35 +1,43 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 export default class ProductDetails {
-    constructor(productId, dataSource) {
-        this.productId = productId;
-        this.product = {};
-        this.dataSource = dataSource;
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+
+    this.renderProductDetails();
+
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", this.addProductToCart.bind(this));
+  }
+
+  addProductToCart() {
+    let cartItems = getLocalStorage("so-cart");
+
+    if (!cartItems) {
+      cartItems = [];
     }
 
-    async init() {
-        this.product = await this.dataSource.findProductById(this.productId);
+    cartItems.push(this.product);
+    setLocalStorage("so-cart", cartItems);
+  }
 
-        this.renderProductDetails();
+  renderProductDetails() {
+    const regularPrice = Number(this.product.SuggestedRetailPrice);
+    const salePrice = Number(this.product.FinalPrice);
 
-        document
-            .getElementById("addToCart")
-            .addEventListener("click", this.addProductToCart.bind(this));
-    }
+    const discountAmount = regularPrice - salePrice;
+    const discountPercent = Math.round(
+      (discountAmount / regularPrice) * 100
+    );
 
-    addProductToCart() {
-        let cartItems = getLocalStorage("so-cart");
-
-        if (!cartItems) {
-            cartItems = [];
-        }
-
-        cartItems.push(this.product);
-        setLocalStorage("so-cart", cartItems);
-    }
-
-    renderProductDetails() {
-        document.querySelector(".product-detail").innerHTML = `
+    document.querySelector(".product-detail").innerHTML = `
       <h3>${this.product.Brand.Name}</h3>
 
       <h2 class="divider">${this.product.NameWithoutBrand}</h2>
@@ -40,9 +48,28 @@ export default class ProductDetails {
         alt="${this.product.Name}"
       />
 
-      <p class="product-card__price">$${this.product.FinalPrice}</p>
+      <p class="product-card__price">
+        $${salePrice.toFixed(2)}
+      </p>
 
-      <p class="product__color">${this.product.Colors[0].ColorName}</p>
+      ${
+        discountAmount > 0
+          ? `
+            <p class="product-detail__original-price">
+              Regular price: $${regularPrice.toFixed(2)}
+            </p>
+
+            <p class="product-detail__discount">
+              Save $${discountAmount.toFixed(2)}
+              (${discountPercent}% off)
+            </p>
+          `
+          : ""
+      }
+
+      <p class="product__color">
+        ${this.product.Colors[0].ColorName}
+      </p>
 
       <p class="product__description">
         ${this.product.DescriptionHtmlSimple}
@@ -52,5 +79,5 @@ export default class ProductDetails {
         <button id="addToCart">Add to Cart</button>
       </div>
     `;
-    }
+  }
 }

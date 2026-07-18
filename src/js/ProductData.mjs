3be6ebf4afc584f29ce963149042dab163d55b6c
Victor <vhenriquez@byupathway.edu>
2026-07-18 19:20:28 -0400

    Fix ProductData to use local JSON fallback when VITE_SERVER_URL is unset

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,27 +1,37 @@
-const baseURL = import.meta.env.VITE_SERVER_URL
+const baseURL = import.meta.env.VITE_SERVER_URL || "";
 
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
-  } else {
-    throw new Error("Bad Response");
   }
+
+  throw new Error("Bad Response");
 }
 
 export default class ProductData {
-  constructor() {
-
-  }
-  async getData(category) {
-    const response = await fetch(`${baseURL}products/search/${category}`);
-    const data = await convertToJson(response);
-    return data.Result;
+  constructor(category = "tents") {
+    this.category = category;
   }
 
-  async findProductById(id) {
-    const response = await fetch(`${baseURL}product/${id}`);
-    const data = await convertToJson(response);
-    console.log(data.Result);
-    return data.Result;
+  async getData(category = this.category) {
+    if (baseURL) {
+      const response = await fetch(`${baseURL}products/search/${category}`);
+      const data = await convertToJson(response);
+      return data.Result;
+    }
+
+    const response = await fetch(`/json/${category}.json`);
+    return await convertToJson(response);
+  }
+
+  async findProductById(id, category = this.category) {
+    if (baseURL) {
+      const response = await fetch(`${baseURL}product/${id}`);
+      const data = await convertToJson(response);
+      return data.Result;
+    }
+
+    const products = await this.getData(category);
+    return products.find((item) => String(item.Id) === String(id));
   }
 }

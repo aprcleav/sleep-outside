1083d25f77984f89452c020d3f47c25a06ad9819
justhens <justhens@gmail.com>
2026-07-22 22:00:26 -0400

    W04 Team Activity: Checkout

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -55,4 +55,21 @@ export default class CheckoutProcess {
     const orderTotal = document.querySelector(`${this.outputSelector} #order-total`);
     orderTotal.innerText = `$${this.orderTotal.toFixed(2)}`;
   }
+
+
+// STEP 6 OF TEAM PROJECT NEEDS TO BE COMPLETED HERE
+
+
+  // takes the items currently stored in the cart (localstorage) and returns them in a simplified form.
+// function packageItems(items) {
+  // convert the list of products from localStorage to the simpler form required for the checkout process.
+  // An Array.map would be perfect for this process.
+
+  async checkout(form) {
+  // get the form element data by the form name
+  // convert the form data to a JSON order object using the formDataToJSON function
+  // populate the JSON order object with the order Date, orderTotal, tax, shipping, and list of items
+  // call the checkout method in the ExternalServices module and send it the JSON order data.
+        
+}
 }

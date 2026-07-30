ca7669cd4dcc5850ade0f3c69c7ac07b0d2dec55
reidsana <oksanareid48@gmail.com>
2026-07-29 23:30:13 -0500

    Add newsletter signup

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+
+const newsletterForm = document.querySelector("#newsletter-form");
+
+if (newsletterForm) {
+    newsletterForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        alertMessage("Thank you for subscribing to our newsletter!", false);
+
+        newsletterForm.reset();
+    });
+}

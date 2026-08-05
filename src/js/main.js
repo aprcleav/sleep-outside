584025c65095a72efbf8ceb52fb23f3dd70897b9
reidsana <oksanareid48@gmail.com>
2026-08-05 17:29:23 -0500

    Add first visit registration giveaway popup

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, alertMessage } from "./utils.mjs";
 
 loadHeaderFooter();
 
+// Newsletter form
 const newsletterForm = document.querySelector("#newsletter-form");
 
 if (newsletterForm) {
@@ -11,5 +12,20 @@ if (newsletterForm) {
         alertMessage("Thank you for subscribing to our newsletter!", false);
 
         newsletterForm.reset();
+    });
+}
+
+// Welcome modal
+const modal = document.getElementById("welcome-modal");
+const closeBtn = document.getElementById("close-modal");
+
+if (modal && !localStorage.getItem("welcomeModalShown")) {
+    modal.classList.remove("hidden");
+}
+
+if (closeBtn) {
+    closeBtn.addEventListener("click", () => {
+        modal.classList.add("hidden");
+        localStorage.setItem("welcomeModalShown", "true");
     });
 }
